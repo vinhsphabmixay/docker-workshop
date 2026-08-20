668d2e2d9b4bc97ded14c73b30593d9081fbb606
Le-Tien-Vinh SPHABMIXAY <ltvinh24@gmail.com>
2026-08-20 08:15:08 +0000

    test for vscode

---
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata_20192020.sql
@@ -0,0 +1,30 @@
+with source as (
+    select * from {{source('staging', 'green_tripdata_20192020')}}
+),
+
+renamed as (
+    select
+        VendorID,
+        lpep_pickup_datetime,
+        lpep_dropoff_datetime,
+        store_and_fwd_flag,
+        RatecodeID,
+        PULocationID,
+        DOLocationID,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        improvement_surcharge,
+        total_amount,
+        payment_type,
+        trip_type,
+        congestion_surcharge,
+    from source
+)
+
+select * from renamed
