8d4189778fdb404385b9062ae77901e8ca2e2456
Le-Tien-Vinh SPHABMIXAY <ltvinh24@gmail.com>
2026-08-27 17:28:14 +0000

    0827 code

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata_20192020.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata_20192020.sql
@@ -1,34 +1,35 @@
--- depends_on: {{ source('staging', 'green_tripdata_20192020') }}
 {{
     config(
         materialized='view'
     )
 }}
+
 with tripdata as
 (
     SELECT
         *
-        ,ROW_NUMBER() OVER(PARTITION BY vendorid, lpep_pickup_datetime) AS rn
-    FROM {{ source('staging', 'green_tripdata_20192020') }}
+        ,ROW_NUMBER() OVER(PARTITION BY vendorid, tpep_pickup_datetime) AS rn
+    FROM {{ source('staging', 'yellow_tripdata_20192020') }}
     WHERE vendorid IS NOT NULL
 )
 SELECT
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime'])}} as tripid
+    {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime'])}} as tripid
     , cast(vendorid AS INTEGER) as vendorid
     , cast(ratecodeid as integer) as ratecodeid
     , cast(pulocationid as integer) as pickup_locationid
     , cast(dolocationid as integer) as dropoff_locationid
 
     -- timestamps
-    , cast(lpep_pickup_datetime as timestamp) as pickup_datetime
-    , cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime
+    , cast(tpep_pickup_datetime as timestamp) as pickup_datetime
+    , cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime
 
     -- trip info
     , store_and_fwd_flag
     , cast(passenger_count as integer) as passenger_count
     , cast(trip_distance as numeric) as trip_distance
-    , cast(trip_type as integer) as trip_type
+    -- yelow cabs are always street-hail
+    , 1 as trip_type
 
     -- payment info
     , cast(fare_amount as numeric) as fare_amount
@@ -36,7 +37,7 @@ SELECT
     , cast(mta_tax as numeric) as mta_tax
     , cast(tip_amount as numeric) as tip_amount
     , cast(tolls_amount as numeric) as tolls_amount
-    , cast(ehail_fee as numeric) as ehail_fee
+    , cast(0 as numeric) as ehail_fee
     , cast(improvement_surcharge as numeric) as improvement_surcharge
     , cast(total_amount as numeric) as total_amount
     , coalesce(cast(payment_type as integer), 0) as payment_type
