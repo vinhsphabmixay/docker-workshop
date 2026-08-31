e6c2b5cc3961f13e91bc553a9cd5d429abded2dc
Le-Tien-Vinh SPHABMIXAY <ltvinh24@gmail.com>
2026-08-31 17:02:56 +0000

    dfgv

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata_20192020.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata_20192020.sql
@@ -45,7 +45,7 @@ FROM tripdata
 WHERE rn = 1
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
+/*{% if var('is_test_run', default=true) %}
 
     limit 100
 {% endif %}
